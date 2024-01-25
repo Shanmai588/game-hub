f8532d7bd0951ac82b08255ce3bf32c615deaa4a
Andy <g1820781782@gmail.com>
2024-01-26 02:04:05 +1100

    Build the color mode switch with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import ColorModeSwitch from "./components/ColorModeSwitch"
-import GameGrid from "./components/GameGrid"
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import ColorModeSwitch from "./components/ColorModeSwitch";
+import GameGrid from "./components/GameGrid";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div><ColorModeSwitch/><GameGrid></GameGrid></div>
+  return (
+    <Grid
+      templateAreas={{
+        // for moble
+        base: '"nav"    "main"',
+        // for pc
+        lg: '"nav nav" "aside main"',
+      }}
+    >
+      <GridItem area="nav" >
+        <NavBar></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="coral">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="coral">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
