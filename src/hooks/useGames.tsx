9c9806330b2a5418aa39cb407c69e11179242bdc
Andy <g1820781782@gmail.com>
2024-01-31 21:47:02 +1100

    Add game card, change the game grid layout

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api.client";
 import { CanceledError } from "axios";
-interface Game {
+export interface Game {
   id: number;
   name: string;
   // slug: string;
   // games_count:number;
-  // image_background:string;
+  background_image:string;
   // description: string
 }
 
