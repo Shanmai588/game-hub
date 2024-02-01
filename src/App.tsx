a9682b25ba38f073998dbc117243d1e84165f908
Andy <g1820781782@gmail.com>
2024-02-01 19:48:22 +1100

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import ColorModeSwitch from "./components/ColorModeSwitch";
 import GameGrid from "./components/GameGrid";
 import NavBar from "./components/NavBar";
 import GenreList from "./components/GenreList";
@@ -14,12 +13,12 @@ function App() {
         lg: '"nav nav" "aside main"',
       }}
     >
-      <GridItem area="nav" >
+      <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside">
-        <GenreList></GenreList>
+          <GenreList></GenreList>
         </GridItem>
       </Show>
       <GridItem area="main">
