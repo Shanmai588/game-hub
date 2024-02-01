7067983aa80e0524037fdaf3ea3285f185f3f303
Andy <g1820781782@gmail.com>
2024-02-01 18:42:32 +1100

    Add card container for all game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,21 +3,22 @@ import { Card, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 
 const GameGrid = () => {
   const {games, error, isLoading} = useGames();
   const skeletons = ['s1','s2','s3','s4','s5','s6', 's7', 's8', 's9'];
-  skeletons.map((skeleton)=>console.log(skeleton))
-  console.log(isLoading);
   return (
     <>
         {error && <Text>{error}</Text>}
         {/* beaware it should be cloumns not column */}
         <SimpleGrid columns = {{sm:1, md:2,lg:3}} spacing = {10}>
-          {isLoading && skeletons.map((skeleton)=><GameCardSkeleton key = {skeleton}></GameCardSkeleton>)}
+          {isLoading && skeletons.map((skeleton)=><GameCardContainer><GameCardSkeleton key = {skeleton}></GameCardSkeleton></GameCardContainer>)}
           {games.map((game) => (
-            <GameCard key ={game.id} game={game}></GameCard>
+            <GameCardContainer>
+              <GameCard key ={game.id} game={game}></GameCard>
+            </GameCardContainer>
           ))}
         </SimpleGrid>
       
