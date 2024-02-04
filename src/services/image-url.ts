314fcbaa3a58692e8b60484d96a2374e226595af
Andy Guo <z5479311@ad.unsw.edu.au>
2024-02-05 00:11:43 +1100

    handle game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from '../assets/No-Image-Placeholder.webp'
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return noImage;
     const media = 'media/'
     const index = url.indexOf(media) + media.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
