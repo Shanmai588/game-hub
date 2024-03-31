fa9ab13b47b04fe4389f4f38260a5c922a92fc07
Andy Guo <z5479311@ad.unsw.edu.au>
2024-03-31 20:47:20 +1100

    handle errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <NavBar></NavBar>
+      <Box margin={10}>
+        <Heading fontSize={"2xl"}>Oops...</Heading>
+        <Text>
+          {isRouteErrorResponse(error) === true
+            ? "This page does not exist"
+            : "Sorry, an unexpected error occurs"}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
